14804576b02934bca93e3698ad105bd62ccb49f1
AviWaiz <avi.waizman@gmail.com>
2019-06-02 23:37:36 -0700

    Deadlocks

---
+++ src/main/scala/DeadlockDemonstration.scala
@@ -0,0 +1,94 @@
+import java.util.concurrent.CountDownLatch
+
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
+object DeadlockDemonstration {
+  def main(args: Array[String]): Unit = {
+//    val dead = new DeadlockDemonstration
+//    Try(dead.runTest) match {
+//      case Success(value) ⇒ println("Success")
+//      case Failure(exception) ⇒ println("Failed")
+//    }
+
+    val nreLock = new NonReentrantLock()
+    nreLock.lock()
+    print("Got firstLock")
+    // Attempt to get second lock will deadlock self
+    println("Getting second lock")
+    nreLock.lock()
+    println("GotEEM")
+  }
+
+}
+
+class DeadlockDemonstration() {
+  private var counter: Int = 0
+  private val lock1 = new Object()
+  private val lock2 = new Object()
+  val latch = new CountDownLatch(2)
+  implicit val executionContext: ExecutionContextExecutor =
+    ExecutionContext.global
+
+  val f1: Runnable = () =>
+    (0 until 100).foreach(i ⇒ {
+      incrementCounter()
+      println("Incrementing " + i)
+    })
+
+  val f2: Runnable = () ⇒ {
+    (0 until 100).foreach(i ⇒ {
+      decrementCounter()
+      println("Incrementing " + i)
+    })
+  }
+
+  def runTest(): Unit = {
+    val t1 = new Thread(f1)
+    val t2 = new Thread(f2)
+    t1.start()
+    Thread.sleep(100)
+    t2.start()
+    t1.join()
+    t2.join()
+    println("Done : " + counter)
+  }
+
+  def incrementCounter(): Unit = {
+    lock1.synchronized {
+      latch.countDown()
+      println("Acquired lock1")
+      latch.await()
+      lock2.synchronized {
+        counter += 1
+      }
+    }
+    ()
+  }
+  def decrementCounter(): Unit = {
+    lock2.synchronized {
+      println("Acquired lock2")
+      latch.countDown()
+      latch.await()
+      lock1.synchronized {
+        counter -= 1
+      }
+    }
+    ()
+  }
+}
+
+class NonReentrantLock(var isLocked: Boolean = false) {
+
+  def lock(): Unit = synchronized {
+    while(isLocked) {
+      wait()
+    }
+    isLocked = true
+  }
+
+  def unlock(): Unit = synchronized {
+    isLocked = false
+    notify()
+  }
+
+
+}
--- src/main/scala/MergeSort.scala
+++ src/main/scala/MergeSort.scala
@@ -1,17 +1,21 @@
 import scala.annotation.tailrec
+import scala.collection.immutable
 import scala.concurrent.duration._
-import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutor, Future}
+import scala.concurrent.{
+  Await,
+  ExecutionContext,
+  ExecutionContextExecutor,
+  Future
+}
 import scala.util.Random
 object MergeSort {
   implicit val executionContext: ExecutionContextExecutor =
     ExecutionContext.global
   val maxDepth: Int = Runtime.getRuntime.availableProcessors()
   def main(args: Array[String]): Unit = {
-    (10000000 to 10000000).foreach { i ⇒
-      val vec = (0 to i).toList.map((_: Int) ⇒ Random.nextInt(10000000))
-      val a = (0 to i).toVector.map((_: Int) ⇒ Random.nextInt(10000000))
-
-
+    (100000000 to 100000000).foreach { i ⇒
+      val vec = (0 to i).toList.map((_: Int) ⇒ Random.nextInt(100000000))
+//      val a = (0 to i).toVector.map((_: Int) ⇒ Random.nextInt(10000000))
 //      val start = System.currentTimeMillis()
 //      val res = mergeSort(vec, 0, vec.size - 1)
 //      println("Time mergeSort" + (System.currentTimeMillis() - start))
@@ -27,27 +31,28 @@ object MergeSort {
       val res4 = vec.sorted
       println("Time scalaSort " + (System.currentTimeMillis() - start4))
 //      println(res3 == res4)
+//      println(res3)
 //      println(res2 == res4)
 //      println(res == res4)
     }
   }
 
-  def parallelSort(values: List [Int],
-                        start: Int,
-                        end: Int,
-                        depth: Int): Future[List[Int]] = {
-    if (start > end) Future { List() }
-    else if (depth > maxDepth / 2 || start + end < depth || start == end) {
+  def parallelSort(values: List[Int],
+                   start: Int,
+                   end: Int,
+                   depth: Int): Future[List[Int]] = {
+    if (start > end) Future { List() } else if (depth > maxDepth || start + end < depth || start == end) {
       Future { values.sorted }
     } else {
       val step = values.size / maxDepth
-      Future.sequence((0 to values.size + 1 by step).map(start ⇒
-        if (start + step >= values.size)
-        Future {
-          values.slice(start, values.size).sorted
-        }
-        else Future { values.slice(start, start + step)}
-      )).map(_.flatten.toList.sorted)
+      Future
+        .sequence(
+          (0 to values.size + 1 by step).map(start ⇒
+            if (start + step >= values.size)
+              Future {
+                values.slice(start, values.size).sorted
+              } else Future { values.slice(start, start + step) }))
+        .map(_.flatten.sorted.toList)
     }
   }
 
@@ -55,18 +60,17 @@ object MergeSort {
                         start: Int,
                         end: Int,
                         depth: Int): Future[List[Int]] = {
-    if (start > end) Future { List() }
-    else if (depth > maxDepth / 2 || start + end < depth || start == end) {
+    if (start > end) Future { List() } else if (depth > maxDepth / 2 || start + end < depth || start == end) {
       Future { mergeSort(values, start, end) }
     } else {
       val middle = (end - start) / 2 + start
-      val f1: Future[List[Int]] = mergeSortParallel(values, start, middle, depth + 1)
-      val f2: Future[List[Int]] = mergeSortParallel(values, middle + 1, end, depth + 1)
-      f1
-        .flatMap(l1 ⇒
-          f2
-            .map(l2 ⇒ merge(l1, l2))
-        )
+      val f1: Future[List[Int]] =
+        mergeSortParallel(values, start, middle, depth + 1)
+      val f2: Future[List[Int]] =
+        mergeSortParallel(values, middle + 1, end, depth + 1)
+      f1.flatMap(
+          l1 ⇒
+            f2.map(l2 ⇒ merge(l1, l2)))
     }
   }
 
