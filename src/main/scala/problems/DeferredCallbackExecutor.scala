8e337f4d83f3b60aeba9c4f7728afc63aa10daeb
AviWaiz <avi.waizman@gmail.com>
2019-06-26 01:48:14 -0700

    callback code

--- src/main/scala/problems/DeferredCallbackExecutor.scala
+++ src/main/scala/problems/DeferredCallbackExecutor.scala
@@ -1,5 +1,6 @@
 package problems
 
+import java.util.concurrent.TimeUnit
 import java.util.concurrent.locks.{Condition, ReentrantLock}
 
 import scala.collection.mutable
@@ -12,7 +13,35 @@ import scala.collection.mutable
 // ordered by the time remaining to execute.
 // Execution thread can sleep for a duration before the earliest callback to execute in the min-heap is due
 // Threads can register callbacks to the priority queue within a critical section
+// The thread that sleeps according to the closest execution time will need to recalculate it's sleep
+// whenever a new callback is registered
 
+
+// there is a simple callback case class that holds information about execution time
+// there is also  register callback method on the class that makes adds a callback to the queue
+// reentrant lock used for guarding the critical section, which acts as a mutex also introduce a condition variable
+// the execution thread wait on the condition while the thread adding the callback will signal
+
+// lock before critical section
+// all threads trying to register a callback will wait while another thread
+// owns the ReentrantLock
+// waiting threads are signaled in fifo order
+// signal wakes up a a waiting thread on the condition
+// the execution thread won't wake until the thread adding the callback gives up the lock
+
+
+// start method
+// thread enters a perpetual loop
+// 1) initially queue will be empty and the execution thread should just wait
+// indefinitely on the condition variable to be signalled
+
+// 2) when the first callback arrives, we note how many seconds after its arrival does
+// it need to be invoked and await() on the condition variable for that many seconds
+
+// 3) Now two things are possible at this point. No new callbacks arrive, in which case the executor thread completes waiting
+// and polls the queue for tasks that should be executed and starts executing them
+// or a new callback arrives in which case the thread adding a callback would signal the condition variable newCallbackArrived
+// to wake up the execution thread and have it re-evaluate the duration it can sleep for before the earliest callback becomes due
 object DeferredCallbackExecutor {
   // PriorityQueue(min-heap) ordered by executeAt
   def callbackOrder(callback: CallBack): Long = callback.executeAt
@@ -24,12 +53,35 @@ object DeferredCallbackExecutor {
   // Condition to make execution thread wait on
   val newCallbackArrived: Condition = lock.newCondition()
 
-  def start() = {}
+  def start() = {
+    var sleepFor: Long = 0L
+    var lastSeenQSize = 0
+
+    while (true) {
+      lock.lock() // get the lock at the start since there could be a callback in the queue
+
+      while (q.isEmpty) {
+        newCallbackArrived.await() // indefinitely wait while on the condition
+      }
+
+      if (lastSeenQSize == q.size) {
+        newCallbackArrived.await(sleepFor, TimeUnit.MILLISECONDS)
+      }
+
+      // when enough time passed relative to the first element in the queue
+      // get the callback and invoke it
+      val currentTime: Long = System.currentTimeMillis()
+      while (q.nonEmpty && currentTime >= q.head.executeAt) {
+        val callBack: CallBack = q.dequeue()
+        println("Executed at " + System.currentTimeMillis() / 1000 + " required at " + cb.executeAt / 1000 + ": message:" + callBack.message)
+      }
+      sleepFor = if (q.isEmpty) 0 else q.head.executeAt - currentTime
+      lastSeenQSize = q.size
+      lock.unlock()
+    }
+  }
 
   def registerCallback(callBack: CallBack): Unit = {
-    // lock before critical section
-    // all threads trying to register a callback will wait while another thread
-    // owns the ReentrantLock
     lock.lock()
     q.enqueue(callBack)
     newCallbackArrived.signal()
